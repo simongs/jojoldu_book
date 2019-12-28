4deed20ab3fa9f51aff218995a08c28efa489a32
chopokmado <chopokmado@gmail.com>
2019-12-29 00:53:33 +0900

    Initial commit

---
+++ src/main/java/com/dasolute/jojoldo/book/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.dasolute.jojoldo.book.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping
+	public String helloWorld() {
+		return "helloWorld";
+	}
+}
